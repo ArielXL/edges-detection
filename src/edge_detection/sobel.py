25183d89b58671197be3a69673d18ac44b2292b2
ariel.plasencia <arielplasencia00@gmail.com>
2022-10-24 12:34:42 -0400

    update edge detection by sobel operator

--- src/edge_detection/sobel.py
+++ src/edge_detection/sobel.py
@@ -1,43 +1,94 @@
 import cv2
+import pylab
 import numpy as np
+import skimage.filters.edges
 import matplotlib.pyplot as plt
 
-def sobel(image, print=False):
 
+def sobel(image, path='', save=False, show=False):
+    '''
+    Deteccion de bordes mediante el operador de Sobel.
+    '''
     # convertir a imagen en escala de grises
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # gradiente de direccion x
-    sobel_x = cv2.Sobel(image,cv2.CV_64F, 1, 0)
+    sobel_x = cv2.Sobel(image, cv2.CV_64F, 1, 0)
     # gradiente de direccion y
-    sobel_y = cv2.Sobel(image,cv2.CV_64F, 0, 1)
+    sobel_y = cv2.Sobel(image, cv2.CV_64F, 0, 1)
 
     # gradiente de direccion x valor absoluto
     sobel_x = np.uint8(np.absolute(sobel_x))
     # gradiente de direccion y valor absoluto
     sobel_y = np.uint8(np.absolute(sobel_y))
-    
-    sobel_combined = cv2.bitwise_or(sobel_x, sobel_y)
 
-    if print:
-        images = [ image, sobel_x, sobel_y, sobel_combined ]
-        fig, axs = plt.subplots(1, len(images))
+    # sobel1 = cv2.bitwise_or(sobel_x, sobel_y)
+    sobel = skimage.filters.edges.sobel(image)
 
-        for idx,image in enumerate(images):
-            axs[idx].imshow(image, cmap='gray')
+    if show:
+        show_image(sobel)
+        # show_image(sobel1)
+        show_images([image, sobel])
+    if save:
+        name = get_name_image(path)
+        save_image(sobel, f'../../img/{name}_sobel.png')
 
-        plt.show()
+    return sobel
 
-    return sobel_combined
+
+def get_name_image(path):
+    '''
+    Devuelve el nombre de la imagen segun la ruta.
+    '''
+    name_extention = path.split('/')[-1]
+    name, _ = name_extention.split('.')
+    return name
+
+
+def load_image(path):
+    '''
+    Carga la imagen desde la ruta especificada.
+    '''
+    img = cv2.imread(path)
+    return img
+
+
+def show_image(image):
+    '''
+    Muestra la imagen especificada.
+    '''
+    plt.imshow(image, cmap='gray')
+    plt.show()
+
+
+def show_images(images):
+    '''
+    Muestra las imagenes especificadas.
+    '''
+    count_image = len(images)
+    _, axs = plt.subplots(1, count_image)
+
+    for idx, image in enumerate(images):
+        axs[idx].imshow(image, cmap='gray')
+
+    plt.show()
+
+
+def save_image(image, path):
+    '''
+    Guarda la imagen en la ruta especificada.
+    '''
+    pylab.gray()
+    pylab.imsave(path, image)
+
 
 def main():
 
-    path = '../../img/cameraman.tif'
-    image = cv2.imread(path)
+    path = '../../img/lena.tif'
+    image = load_image(path)
 
-    sobel(image, print=True)
+    sobel(image, path=path, save=True, show=True)
+
 
 if __name__ == '__main__':
     main()
- 
- 
