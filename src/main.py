dd8c2955f37cf23456f8d8295d9403545c182842
ariel.plasencia <arielplasencia00@gmail.com>
2022-10-29 18:38:23 -0400

    update main

--- src/main.py
+++ src/main.py
@@ -1,117 +1,99 @@
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-from PIL import Image
-from utils.mse import mse
-from utils.psnr import psnr
-from utils.ssim import ssim
+from utils.images import Images
+
+from quality_measures.mse import mse
+from quality_measures.psnr import psnr
+from quality_measures.ssim import ssim
+from quality_measures.michelson import michelson
+
 from edge_detection.canny import canny
 from edge_detection.sobel import sobel
+from edge_detection.farid import farid
+from edge_detection.scharr import scharr
 from edge_detection.laplace import laplace
+from edge_detection.prewitt import prewitt
+from edge_detection.roberts import roberts
 
-class HomeWork2:
 
-    def __init__(self, image, outskirt='canny'):
+class EdgeDetection:
+
+    def __init__(self, path='../img/cameraman.tif', save=False, show=False):
         '''
         Inicializa las variables a utilizar y llev a cabo el pipeline.
         '''
         self.methods_outskirts = {
-            'canny'     : canny,
-            'laplace'   : laplace,
-            'sobel'     : sobel
+            'canny': canny,
+            'farid': farid,
+            'laplace': laplace,
+            'prewitt': prewitt,
+            'scharr': scharr,
+            'roberts': roberts,
+            'sobel': sobel
         }
+        self.path = path
+        self.save = save
+        self.show = show
+        self.image = Images.LoadImage(self.path)
+
+    def run_edge_detection(self, outskirt='canny'):
 
         if outskirt not in self.methods_outskirts:
-            raise Exception(f"El metodo '{outskirt}' no esta definido para la deteccion de bordes.")
-        
+            raise Exception(
+                f"El método '{outskirt}' no está definido para la detección de bordes.")
+
         self.outskirt = self.methods_outskirts[outskirt]
-        self.image = image
         self.image_outskirt = self.outskirt(self.image)
 
-        self.show_images([ self.image, self.image_outskirt ])
-        self.new_image = self.sum_image(self.image, self.image_outskirt)
-        self.print_results()
+        if self.save:
+            name = Images.GetNameImage(self.path)
+            Images.SaveImage(self.image_outskirt,
+                             f'../img/{name}_{outskirt}.png')
 
-    def show_image(self, image):
-        '''
-        Muestra la imagen especificada.
-        '''
-        plt.imshow(image, cmap='gray')
-        plt.show()
-
-    def show_images(self, images):
-        '''
-        Muestra las imagenes especificadas.
-        '''
-        count_image = len(images)
-        fig, axs = plt.subplots(1, count_image)
-
-        for idx,image in enumerate(images):
-            axs[idx].imshow(image, cmap='gray')
-
-        plt.show()
-
-    def save_image(self, image, path):
-        '''
-        Guarda la imagen en la ruta especificada.
-        '''
-        cv2.imwrite(path, image)
-
-    def get_image(self, pixels):
-        '''
-        Convierte de una matriz a imagen.
-        '''
-        array = np.array(pixels, dtype=np.uint8)
-        image = Image.fromarray(array)
-        return image
-
-    def get_pixels(self, image):
-        '''
-        Devuelve la imagen en forma matricial.
-        '''
-        pixels = np.asarray(image)
-        return pixels
-
-    def sum_image(self, img1, img2):
-        '''
-        Efectua la suma de las imagenes especificadas.
-        '''
-        pixels_img1 = self.get_pixels(img1)
-        pixels_img2 = self.get_pixels(img2)
-        new_pixels = []
-        rows, columns = len(pixels_img1), len(pixels_img1[0])
-
-        for i in range(rows):
-            new_pixels.append([])
-            for j in range(columns):
-                values = pixels_img1[i][j] + pixels_img2[i][j]
-                values = [ 255 if value > 255 else value for value in values ]
-                new_pixels[i].append(values)
-
-        image = self.get_image(new_pixels)
-        # self.show_image(image)
-        return image
+        if self.show:
+            Images.ShowImage(self.image_outskirt)
+            Images.ShowImages([self.image, self.image_outskirt])
 
     def print_results(self):
         '''
         Imprime los resultados de las medidas de contraste 
         de calidad segun las imagenes procesadas.
         '''
-        mse_value = mse(self.image, np.array(self.new_image))
-        psnr_value = psnr(self.image, np.array(self.new_image))
-        ssim_value = ssim(self.image, np.array(self.new_image))
+        new_image = Images.GetSumImage(self.image, self.image_outskirt)
+        arr = np.array(new_image)
 
-        print('El error cuadratico medio (MSE) es %0.3f.' % mse_value)
+        mse_value = mse(self.image, arr)
+        psnr_value = psnr(self.image, arr)
+        ssim_value = ssim(self.image, arr)
+        michelson_value = michelson(self.image, arr)
+
+        print('El error cuadrático medio (MSE) es %0.3f.' % mse_value)
         print('El valor PSNR es %0.3f dB.' % psnr_value)
-        print('El indice de similitud estructural (SSIM) es %0.3f.' % ssim_value)
+        # print('El índice de similitud estructural (SSIM) es %0.3f.' % ssim_value)
+        print('El contraste de Michelson es %0.3f.' % michelson_value)
+
+
+def run(paths, operators):
+
+    for path in paths:
+        edge_detection = EdgeDetection(path=path, save=True, show=False)
+        for operator in operators:
+            edge_detection.run_edge_detection(outskirt=operator)
+            edge_detection.print_results()
+
 
 def main():
-    
-    path = '../img/cameraman.tif'
-    img = cv2.imread(path)
 
-    HomeWork2(img, outskirt='canny')
+    paths = ['../img/cameraman.tif', '../img/house.tif',
+             '../img/lake.tif', '../img/lena.tif',
+             '../img/livingroom.tif', '../img/peppers.tif',
+             '../img/pirate.tif', '../img/jetplane.tif',
+             '../img/mandril.tif']
+    operators = ['canny', 'farid', 'laplace',
+                 'prewitt', 'roberts', 'scharr', 'sobel']
+
+    run(paths, operators)
+
 
 if __name__ == '__main__':
     main()
